49218f057c3188d4faaae2a46fd283df495dfc38
maxelonej <maxelone.py@gmail.com>
2024-03-09 18:07:22 +0400

    Disable scrolling

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,14 @@
 const burger = document.querySelector('.burger')
 const open = document.querySelector('.burger-open') 
 const close = document.querySelector('.burger-close')
+const body = document.getElementById('body')
 
 burger.addEventListener('click', () => {
   open.classList.add('active')
+  body.style.overflowY = 'hidden'
 })
 
 close.addEventListener('click', () => {
   open.classList.remove('active')
+  body.style.overflowY = 'scroll'
 })
