8a023fc55a52a081a716bfe8577a64cb7fb1f2f6
maxelonej <maxelone.py@gmail.com>
2024-03-10 11:21:30 +0400

    Change variable names

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,14 @@
 const burger = document.querySelector('.burger')
-const open = document.querySelector('.burger-open') 
-const close = document.querySelector('.burger-close')
-const body = document.getElementById('body')
+const openBurger = document.querySelector('.burger-open') 
+const closeBurger = document.querySelector('.burger-close')
+const bodyBurger = document.getElementById('body')
 
 burger.addEventListener('click', () => {
-  open.classList.add('active')
-  body.style.overflowY = 'hidden'
+  openBurger.classList.add('active')
+  bodyBurger.style.overflowY = 'hidden'
 })
 
-close.addEventListener('click', () => {
-  open.classList.remove('active')
-  body.style.overflowY = 'scroll'
+closeBurger.addEventListener('click', () => {
+  openBurger.classList.remove('active')
+  bodyBurger.style.overflowY = 'scroll'
 })
