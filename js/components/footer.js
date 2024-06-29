8c79dd2ef9f24d56582b74f73cdc762fc87345cd
maxelonej <maxelone@yandex.ru>
2024-06-29 21:00:00 +0400

    Add footer

---
+++ js/components/footer.js
@@ -0,0 +1,42 @@
+const footers = document.querySelectorAll("footer.footer");
+
+footers.forEach((footer) => {
+  footer.innerHTML = `
+    <div class="footer__wrapper">
+      <a class="footer__link activate-popup">Обратная связь</a>
+      <p class="footer__copyright footer__paragraph">© ${new Date().getFullYear()}, <a class="footer__link" href="/">КДЯ</a></p>
+    </div>
+
+    <div class="popup">
+      <div class="popup__wrapper">
+        <form action="/submit-form" method="post">
+          <fieldset class="popup__fieldset">
+            <textarea name="popup__message" id="popup__message" class="popup__message"
+              placeholder="Введите сообщение"></textarea>
+            <button class="popup__button" type="submit">Отправить</button>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  `;
+});
+
+const popup = document.querySelector(".popup");
+const link = document.querySelector(".activate-popup");
+const popupWrapper = document.querySelector(".popup__wrapper");
+
+link.addEventListener("click", (e) => {
+  e.stopPropagation();
+  popup.classList.toggle("active");
+  document.body.style.overflowY = "hidden";
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !popupWrapper.contains(event.target) &&
+    popup.classList.contains("active")
+  ) {
+    document.body.style.overflowY = "scroll";
+    popup.classList.remove("active");
+  }
+});
