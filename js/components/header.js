2fd876bdd4bef66fa8ffb814c49802a1280a8a2c
maxelonej <maxelone@yandex.ru>
2024-07-12 03:07:51 +0400

    Move global.js code into header.js

--- js/components/header.js
+++ js/components/header.js
@@ -103,3 +103,13 @@ document.addEventListener("click", (e) => {
     burgerContent.classList.remove("active");
   }
 });
+
+// set navigation link active
+const currentPage = window.location.pathname.split("/").pop();
+const navLinks = document.querySelectorAll(".header__nav-link");
+
+navLinks.forEach((link) => {
+  if (link.href.includes(currentPage)) {
+    link.classList.add("active");
+  }
+});
--- js/global.js
+++
@@ -1,9 +0,0 @@
-// set navigation link active
-const currentPage = window.location.pathname.split("/").pop();
-const navLinks = document.querySelectorAll(".header__nav-link");
-
-navLinks.forEach((link) => {
-  if (link.href.includes(currentPage)) {
-    link.classList.add("active");
-  }
-});
