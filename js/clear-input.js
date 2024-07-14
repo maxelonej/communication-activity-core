0913d8f63b05bf14e715fff700df8a9c3dc65898
maxelonej <maxelone@yandex.ru>
2024-07-14 11:18:33 +0400

    Add input scripts

---
+++ js/clear-input.js
@@ -0,0 +1,39 @@
+const searchInput = document.querySelector(".search-input");
+const clearIcon = document.querySelector(
+  ".search-competition img:nth-child(3)"
+);
+
+searchInput.addEventListener("input", () => {
+  filterCompetencies(searchInput.value);
+  if (searchInput.value !== "") {
+    clearIcon.classList.add("active");
+  } else {
+    clearIcon.classList.remove("active");
+  }
+});
+
+searchInput.addEventListener("blur", () => {
+  if (searchInput.value === "") {
+    clearIcon.classList.remove("active");
+  }
+});
+
+clearIcon.addEventListener("click", () => {
+  clearIcon.classList.remove("active");
+  searchInput.value = "";
+  // searchInput.focus();
+  filterCompetencies(""); // reset filtering
+});
+
+const options = document.querySelectorAll(".option");
+
+const filterCompetencies = (searchValue) => {
+  options.forEach((option) => {
+    const label = option.querySelector(".text").textContent.toLowerCase();
+    if (label.includes(searchValue.toLowerCase())) {
+      option.style.display = "block";
+    } else {
+      option.style.display = "none";
+    }
+  });
+};
