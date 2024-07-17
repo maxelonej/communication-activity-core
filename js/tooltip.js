8ebe2d47b0e2c9c3361338f7f7c369bc06133501
maxelonej <maxelone@yandex.ru>
2024-07-17 09:18:23 +0400

    Change variable name

--- js/tooltip.js
+++ js/tooltip.js
@@ -1,10 +1,8 @@
-const projectContainers = document.querySelectorAll(
-  '.container[data-type="project"]'
-);
+const projectContainers = document.querySelectorAll('.container[data-type="project"]');
 
-const mq = window.matchMedia("(min-width: 1024px)");
+let mediaQueryList = window.matchMedia("(min-width: 1024px)");
 
-if (mq.matches) {
+if (mediaQueryList.matches) {
   projectContainers.forEach((container) => {
     const tooltipContainer = container.querySelector(".tooltip-container");
     const link = container.querySelector(".button-link");
@@ -13,16 +11,12 @@ if (mq.matches) {
       fetch(link.href)
         .then((response) => response.text())
         .then((html) => {
-          const projectIntroContainer = new DOMParser()
-            .parseFromString(html, "text/html")
-            .querySelector('.container[data-type="project-intro"]');
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(html, "text/html");
+          const projectIntroContainer = doc.querySelector('.container[data-type="project-intro"]');
           if (projectIntroContainer) {
             const tooltipContent = projectIntroContainer.innerHTML;
-            if (tooltipContent !== "") {
-              tooltipContainer.innerHTML = tooltipContent;
-              // Show the tooltip only if it has content
-              tooltipContainer.style.display = "block";
-            }
+            tooltipContainer.innerHTML = tooltipContent;
           }
         });
     });
