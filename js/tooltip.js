aad82144e55b4327c3783b68517cb9f1b720817e
maxelonej <maxelone@yandex.ru>
2024-07-17 09:06:06 +0400

    Add script works on min-windth 1024 only

--- js/tooltip.js
+++ js/tooltip.js
@@ -2,27 +2,29 @@ const projectContainers = document.querySelectorAll(
   '.container[data-type="project"]'
 );
 
-projectContainers.forEach((container) => {
-  const tooltipContainer = container.querySelector(".tooltip-container");
-  const link = container.querySelector(".button-link");
+const mq = window.matchMedia("(min-width: 1024px)");
 
-  container.addEventListener("mouseover", () => {
-    fetch(link.href)
-      .then((response) => response.text())
-      .then((html) => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, "text/html");
-        const projectIntroContainer = doc.querySelector(
-          '.container[data-type="project-intro"]'
-        );
-        if (projectIntroContainer) {
-          tooltipContainer.innerHTML = projectIntroContainer.innerHTML;
-        } else {
-          console.error("Failed to extract project intro container");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching content:", error);
-      });
+if (mq.matches) {
+  projectContainers.forEach((container) => {
+    const tooltipContainer = container.querySelector(".tooltip-container");
+    const link = container.querySelector(".button-link");
+
+    container.addEventListener("mouseover", () => {
+      fetch(link.href)
+        .then((response) => response.text())
+        .then((html) => {
+          const projectIntroContainer = new DOMParser()
+            .parseFromString(html, "text/html")
+            .querySelector('.container[data-type="project-intro"]');
+          if (projectIntroContainer) {
+            const tooltipContent = projectIntroContainer.innerHTML;
+            if (tooltipContent !== "") {
+              tooltipContainer.innerHTML = tooltipContent;
+              // Show the tooltip only if it has content
+              tooltipContainer.style.display = "block";
+            }
+          }
+        });
+    });
   });
-});
+}
