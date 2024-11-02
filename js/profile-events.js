57c299e2e6fff46e837d2e34ac112365cb3638ed
maxelonej <maxelone@yandex.ru>
2024-11-03 00:14:22 +0400

    Remove comments

--- js/profile-events.js
+++ js/profile-events.js
@@ -7,23 +7,18 @@ if (buttons.length && cards.length) {
       const tag = button.getAttribute("data-tag");
       const isActive = button.getAttribute("data-type") === "active";
 
-      // Убираем активный класс с всех кнопок
       buttons.forEach((btn) => btn.setAttribute("data-type", "inactive"));
 
-      // Если кнопка была неактивной, активируем её
       if (!isActive) {
         button.setAttribute("data-type", "active");
       }
 
-      // Фильтруем карточки
       cards.forEach((card) => {
         const cardTags = card.getAttribute("data-tags").split(" ");
         if (isActive || cardTags.includes(tag)) {
-          // Показываем карточки с анимацией
           animateCard(card, true);
         } else {
-          // Скрываем карточки, которые не соответствуют выбранному тегу
-          card.style.display = "none"; // Скрываем карточку сразу
+          card.style.display = "none";
         }
       });
     });
@@ -32,10 +27,9 @@ if (buttons.length && cards.length) {
 
 function animateCard(card, show) {
   if (show) {
-    card.style.display = "flex"; // Убедитесь, что карточка отображается
-    card.style.opacity = 0; // Устанавливаем начальную непрозрачность
+    card.style.display = "flex";
+    card.style.opacity = 0;
 
-    // Используем requestAnimationFrame для обновления стиля
     requestAnimationFrame(() => {
       card.animate(
         [
@@ -47,7 +41,7 @@ function animateCard(card, show) {
           easing: "ease-in-out",
         }
       );
-      card.style.opacity = 1; // Устанавливаем окончательную непрозрачность
+      card.style.opacity = 1;
     });
   }
 }
