a78254084836de9ada6e4c5cea21a1d5fe2aa601
Maxelone <maxelone@yandex.ru>
2025-02-17 00:35:53 +0400

    Add rounded button

--- frontend/src/components/shared/Button.jsx
+++ frontend/src/components/shared/Button.jsx
@@ -1,5 +1,7 @@
-function Button({ text }) {
-  return <button className="button">{text}</button>;
+function Button({ text, rounded }) {
+  return (
+    <button className={`button ${rounded ? "rounded" : ""}`}>{text}</button>
+  );
 }
 
 export default Button;
