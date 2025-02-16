c2bb909d66282098402d16114a3dea20fd0e9332
Maxelone <maxelone@yandex.ru>
2025-02-16 21:13:42 +0400

    Add app components

---
+++ frontend/src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import Header from "../widgets/Header";
+import Hero from "../widgets/Hero";
+
+import "../../styles/index.scss";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
