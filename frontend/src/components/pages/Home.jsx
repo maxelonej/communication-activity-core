e7206c539a5c7e022ca24bfa1c37d060b1a77c78
Maxelone <maxelone@yandex.ru>
2025-02-17 12:29:10 +0400

    Add pages

---
+++ frontend/src/components/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../widgets/Header";
+import Hero from "../widgets/Hero";
+import ProjectsPreview from "../widgets/ProjectsPreview";
+import Footer from "../widgets/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <ProjectsPreview />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
