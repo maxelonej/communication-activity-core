81d3a5394d1009a15f362e7cb1d716a159d27706
Maxelone <maxelone@yandex.ru>
2025-02-17 21:27:05 +0400

    Update path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/communication-activity-core/",
   plugins: [react()],
-  base: "/communication-activity-core",
 });
