35e2c9cafca2db6eb055d905455e62da2d039e83
Maxelone <maxelone@yandex.ru>
2025-02-17 21:25:22 +0400

    Update header links path

--- frontend/src/components/widgets/Header.jsx
+++ frontend/src/components/widgets/Header.jsx
@@ -5,20 +5,28 @@ function Header() {
     <>
       <header className="header">
         <div className="container">
-          <img className="logo" src={logo} alt="Лого" draggable="false" />
+          <a href="#/">
+            <img
+              className="logo"
+              src={logo}
+              alt="Главная"
+              title="Главная"
+              draggable="false"
+            />
+          </a>
           <ul className="header__menu">
             <li className="menu__item">
-              <a className="menu__link" href="#">
+              <a className="menu__link" href="#/internships">
                 Практики и стажировки
               </a>
             </li>
             <li className="menu__item">
-              <a className="menu__link" href="#">
+              <a className="menu__link" href="#/projects">
                 Биржа проектов
               </a>
             </li>
             <li className="menu__item">
-              <a className="menu__link" href="#">
+              <a className="menu__link" href="#/pas">
                 ПАС
               </a>
             </li>
