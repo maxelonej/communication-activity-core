6a7669f8bb468041b6795f811a9a0093a9498d98
Maxelone <maxelone@yandex.ru>
2025-02-17 21:24:06 +0400

    Add gh-pages router, add pages

--- frontend/src/components/app/App.jsx
+++ frontend/src/components/app/App.jsx
@@ -1,12 +1,21 @@
+import { HashRouter, Routes, Route } from "react-router"; // instead of browserrouter for gh-pages
 import Home from "../pages/Home";
+import Projects from "../pages/Projects";
+import PAS from "../pages/PAS";
+import Internships from "../pages/Internships";
 
 import "../../styles/index.scss";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pas" element={<PAS />} />
+        <Route path="/internships" element={<Internships />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
