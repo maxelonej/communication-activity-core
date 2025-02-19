96c5803452c880729bc46cee2b02c65e87ae78d9
Maxelone <maxelone@yandex.ru>
2025-02-19 11:19:00 +0400

    Center NotFound page content

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -1,10 +1,12 @@
 function NotFound() {
   return (
-    <div className="container">
-      <h1>Страница не найдена</h1>
-      <a className="button" href="#/">
-        Вернуться на главную
-      </a>
+    <div className="container error">
+      <div>
+        <h1>Страница не найдена</h1>
+        <a className="button" href="#/">
+          Вернуться на главную
+        </a>
+      </div>
     </div>
   );
 }
